8a5df3453ddaddb622ede1df7f5142c421c6f6b5
Axel Beckert <abe@deuxchevaux.org>
2022-03-16 21:01:07 +0000

    Support underlined text (as bold+italic)
    Mattermost doesn't seem to support underlined text, so let's use the
    rather uncommon combination of bold and italic text.

--- telegram2mm.pl
+++ telegram2mm.pl
@@ -102,6 +102,10 @@ sub transform_msg {
 			'_'.$text_element->{text}.'_';
 		    } elsif (exists($text_element->{type}) and
 			     exists($text_element->{text}) and
+			     $text_element->{type} eq 'underline') {
+			'**_'.$text_element->{text}.'_**';
+		    } elsif (exists($text_element->{type}) and
+			     exists($text_element->{text}) and
 			     $text_element->{type} eq 'pre') {
 			"\n".'```'."\n".$text_element->{text}."\n".'```'."\n";
 		    } elsif (exists($text_element->{type}) and
